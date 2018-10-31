518c99834c57f02db5485276b813165106a71fec
Anirudh Ghantasala <lghantas@purdue.edu>
2018-10-30 23:10:01 -0400

    started on keras vgg16 implementation for transfer learning

---
+++ AI Project/VGG16_Keras/vgg16_transfer.py
@@ -0,0 +1,188 @@
+
+import matplotlib.pyplot as plt
+import PIL
+import tensorflow as tf
+import numpy as np
+import os
+
+from tensorflow.python.keras.models import Model, Sequential
+from tensorflow.python.keras.layers import Dense, Flatten, Dropout
+from tensorflow.python.keras.applications import VGG16
+from tensorflow.python.keras.applications.vgg16 import preprocess_input, decode_predictions
+from tensorflow.python.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.python.keras.optimizers import Adam, RMSprop
+
+def path_join(dirname, filenames):
+    return [os.path.join(dirname, filename) for filename in filenames]
+
+
+def plot_images(images, cls_true, cls_pred=None, smooth=True):
+    assert len(images) == len(cls_true)
+
+    # Create figure with sub-plots.
+    fig, axes = plt.subplots(3, 3)
+
+    # Adjust vertical spacing.
+    if cls_pred is None:
+        hspace = 0.3
+    else:
+        hspace = 0.6
+    fig.subplots_adjust(hspace=hspace, wspace=0.3)
+
+    # Interpolation type.
+    if smooth:
+        interpolation = 'spline16'
+    else:
+        interpolation = 'nearest'
+
+    for i, ax in enumerate(axes.flat):
+        # There may be less than 9 images, ensure it doesn't crash.
+        if i < len(images):
+            # Plot image.
+            ax.imshow(images[i],
+                      interpolation=interpolation)
+
+            # Name of the true class.
+            cls_true_name = class_names[cls_true[i]]
+
+            # Show true and predicted classes.
+            if cls_pred is None:
+                xlabel = "True: {0}".format(cls_true_name)
+            else:
+                # Name of the predicted class.
+                cls_pred_name = class_names[cls_pred[i]]
+
+                xlabel = "True: {0}\nPred: {1}".format(cls_true_name, cls_pred_name)
+
+            # Show the classes as the label on the x-axis.
+            ax.set_xlabel(xlabel)
+
+        # Remove ticks from the plot.
+        ax.set_xticks([])
+        ax.set_yticks([])
+
+    # Ensure the plot is shown correctly with multiple plots
+    # in a single Notebook cell.
+    plt.show(block=True)
+
+
+# Import a function from sklearn to calculate the confusion-matrix.
+from sklearn.metrics import confusion_matrix
+
+
+def print_confusion_matrix(cls_pred):
+    # cls_pred is an array of the predicted class-number for
+    # all images in the test-set.
+
+    # Get the confusion matrix using sklearn.
+    cm = confusion_matrix(y_true=cls_test,  # True class for test-set.
+                          y_pred=cls_pred)  # Predicted class.
+
+    print("Confusion matrix:")
+
+    # Print the confusion matrix as text.
+    print(cm)
+
+    # Print the class-names for easy reference.
+    for i, class_name in enumerate(class_names):
+        print("({0}) {1}".format(i, class_name))
+
+
+def plot_example_errors(cls_pred):
+    # cls_pred is an array of the predicted class-number for
+    # all images in the test-set.
+
+    # Boolean array whether the predicted class is incorrect.
+    incorrect = (cls_pred != cls_test)
+
+    # Get the file-paths for images that were incorrectly classified.
+    image_paths = np.array(image_paths_test)[incorrect]
+
+    # Load the first 9 images.
+    images = load_images(image_paths=image_paths[0:9])
+
+    # Get the predicted classes for those images.
+    cls_pred = cls_pred[incorrect]
+
+    # Get the true classes for those images.
+    cls_true = cls_test[incorrect]
+
+    # Plot the 9 images we have loaded and their corresponding classes.
+    # We have only loaded 9 images so there is no need to slice those again.
+    plot_images(images=images,
+                cls_true=cls_true[0:9],
+                cls_pred=cls_pred[0:9])
+
+def example_errors():
+    # The Keras data-generator for the test-set must be reset
+    # before processing. This is because the generator will loop
+    # infinitely and keep an internal index into the dataset.
+    # So it might start in the middle of the test-set if we do
+    # not reset it first. This makes it impossible to match the
+    # predicted classes with the input images.
+    # If we reset the generator, then it always starts at the
+    # beginning so we know exactly which input-images were used.
+    generator_test.reset()
+
+    # Predict the classes for all images in the test-set.
+    y_pred = new_model.predict_generator(generator_test,
+                                         steps=steps_test)
+
+    # Convert the predicted classes from arrays to integers.
+    cls_pred = np.argmax(y_pred, axis=1)
+
+    # Plot examples of mis-classified images.
+    plot_example_errors(cls_pred)
+
+    # Print the confusion matrix.
+    print_confusion_matrix(cls_pred)
+
+def load_images(image_paths):
+    # Load the images from disk.
+    images = [plt.imread(path) for path in image_paths]
+
+    # Convert to a numpy array and return it.
+    return np.asarray(images)
+
+
+def plot_training_history(history):
+    # Get the classification accuracy and loss-value
+    # for the training-set.
+    acc = history.history['categorical_accuracy']
+    loss = history.history['loss']
+
+    # Get it for the validation-set (we only use the test-set).
+    val_acc = history.history['val_categorical_accuracy']
+    val_loss = history.history['val_loss']
+
+    # Plot the accuracy and loss-values for the training-set.
+    plt.plot(acc, linestyle='-', color='b', label='Training Acc.')
+    plt.plot(loss, 'o', color='b', label='Training Loss')
+
+    # Plot it for the test-set.
+    plt.plot(val_acc, linestyle='--', color='r', label='Test Acc.')
+    plt.plot(val_loss, 'o', color='r', label='Test Loss')
+
+    # Plot title and legend.
+    plt.title('Training and Test Accuracy')
+    plt.legend()
+
+    # Ensure the plot shows correctly.
+    plt.show()
+
+
+model = VGG16(include_top=True, weights='imagenet')
+
+input_shape = model.layers[0].output_shape[1:3]
+print(input_shape)
+
+
+
+
+
+
+
+
+
+
+
